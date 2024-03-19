ae03411d8424e0f38d75c42c6204cd161b44bd69
Enrico <enrico.mautone@remira.com>
2024-03-19 16:55:56 +0100

    log added

--- appy/appy_service.py
+++ appy/appy_service.py
@@ -10,6 +10,7 @@ def run_server():
     service_ip = os.getenv("APPY_SERVICE_IP", "default_ip")
     service_port = os.getenv("APPY_SERVICE_PORT", "default_port")
 
+    print(f"Starting appy server on ip %s port %s" % (service_ip,service_port) ) 
     uvicorn.run(app, host=service_ip, port=service_port)
 
 if __name__ == "__main__":
