1f18967f8fc37dbda73ef224feb53638015556d2
Enrico <enrico.mautone@remira.com>
2024-03-19 16:55:46 +0100

    modified to set env variables on os

--- decl_config_vars_win.bat
+++ decl_config_vars_win.bat
@@ -1,4 +1,13 @@
-set SERVICE_IP="127.0.0.1"
-set SERVICE_PORT="8000"
-set APPY_CONFIG_PATH="./config.py"
+@echo off
+REM Imposta le variabili di ambiente permanentemente per l'utente corrente
 
+REM Imposta APPY_CONFIG_PATH
+setx APPY_CONFIG_PATH ".\config.py"
+
+REM Imposta SERVICE_IP
+setx APPY_SERVICE_IP "127.0.0.1"
+
+REM Imposta SERVICE_PORT
+setx APPY_SERVICE_PORT "8000"
+
+echo Le variabili di ambiente sono state impostate.
