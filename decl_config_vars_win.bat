b8d33b9165c5ade7aeea8ec2369b6d6bf7626f80
Enrico <enrico.mautone@remira.com>
2024-03-19 18:03:07 +0100

    updated with all config variables

--- decl_config_vars_win.bat
+++ decl_config_vars_win.bat
@@ -1,13 +1,19 @@
 @echo off
 REM Imposta le variabili di ambiente permanentemente per l'utente corrente
 
-REM Imposta APPY_CONFIG_PATH
+
 setx APPY_CONFIG_PATH ".\config.py"
 
-REM Imposta SERVICE_IP
 setx APPY_SERVICE_IP "127.0.0.1"
 
-REM Imposta SERVICE_PORT
+
 setx APPY_SERVICE_PORT "8000"
+
+setx APPY_SECRET_KEY "YOUR_SECRET_KEY"
+setx APPY_ALGORITHM "HS256"
+setx APPY_VERIFY_TOKEN False
+setx DATABASE_URL "mssql+pyodbc://sa:password01!@WINDELL-186CUHK\SQLEXPRESS/NAUS_PROD?driver=ODBC+Driver+17+for+SQL+Server"
+setx SCHEMA "dbo"
+setx TABLES "affitti,clienti"
 
 echo Le variabili di ambiente sono state impostate.
